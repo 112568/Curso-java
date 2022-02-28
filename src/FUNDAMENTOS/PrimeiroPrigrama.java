850ad6a61809a4bfd0c6fa337a94cafc2a5d9fc2
Jaoramos <jaoramos123@gmail.com>
2022-02-28 20:12:25 -0300

    first commit

---
+++ src/FUNDAMENTOS/PrimeiroPrigrama.java
@@ -0,0 +1,10 @@
+package FUNDAMENTOS;
+
+public class PrimeiroPrigrama {
+
+	public static void main(String[] args) {
+		
+		// Uma sentença de código termina com ;
+		System.out.println("Primeiro programa!!!");
+	}
+}
