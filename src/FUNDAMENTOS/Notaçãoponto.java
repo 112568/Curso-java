53a1943913d3cd2daaf63059d93ae74fc009fc6b
Jaoramos <jaoramos123@gmail.com>
2022-03-02 10:56:58 -0300

    exercicio

---
+++ src/FUNDAMENTOS/Notaçãoponto.java
@@ -0,0 +1,31 @@
+package FUNDAMENTOS;
+
+import java.lang.invoke.StringConcatException;
+import java.security.DigestOutputStream;
+
+public class Notaçãoponto {
+	
+	public static void main(String[] args) {
+		
+		String s = "Bom dia X";
+		s = s.replace("X", "Senhora");
+	    s = s.toUpperCase();
+	    s = s.concat("!!!");
+	    
+		System.out.println(s);
+		
+		System.out.println("Leo" .toUpperCase());
+		
+		String y = "Bom dia X"
+				.replace("X", "Gui")
+				.toUpperCase()
+				.concat("!!!");
+		System.out.println(y);
+		
+		// Tipos primitivos não tem o operador "."
+		int a = 3;
+		System.out.println(a);
+	    
+	}
+
+}
